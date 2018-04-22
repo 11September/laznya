e01b9838a2816f7f7fd0d661f8648a337118136a
nosslsvec8 <nosiljevets@gmail.com>
2018-04-22 13:22:25 +0300

    add. header якоря

--- js/common.js
+++ js/common.js
@@ -18,6 +18,21 @@ $(document).ready(function() {
         //анимируем переход на расстояние - top за 1500 мс
         $('body,html').animate({scrollTop: top}, 1500);
     });
+
+    $(".menu_links").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор блока с атрибута href
+        var id  = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+        //alert(id);
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
 });
 
 
